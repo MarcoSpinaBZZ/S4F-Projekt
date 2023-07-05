98eb4990e10ada25b64b1b19225576307982f437
MarcoSpinaBZZ <spinam@bzz.ch>
2023-07-05 09:24:34 +0200

    add tic-tac-toe jsx

---
+++ s4f-project/pages/tic-tac-toe/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./tic-tac-toe.module.css"
+
+function page() {
+  return (
+    <div className={styles.test}>Tic-Tac-Toe</div>
+  )
+}
+
+export default page
