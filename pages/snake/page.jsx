39181685fdf79b095684423902ac3721531813e8
MarcoSpinaBZZ <spinam@bzz.ch>
2023-07-05 14:45:33 +0200

    add snake code

--- pages/snake/page.jsx
+++ pages/snake/page.jsx
@@ -1,51 +1,162 @@
-import Image from 'next/image';
+import React, { useState, useRef, useEffect } from "react";
 import styles from "./snake.module.css";
 import { useRouter } from 'next/router';
-import { useState } from 'react';
 
-const Snake = () => {
-  const handleClick = () => {
-    router.push('/lounge/page'); // Replace with the correct path to the desired page
+
+const CANVAS_SIZE = [800, 800];
+const SNAKE_START = [
+  [8, 7],
+  [8, 8]
+];
+const APPLE_START = [8, 3];
+const SCALE = 40;
+const SPEED = 100;
+const DIRECTIONS = {
+  38: [0, -1], // up
+  40: [0, 1], // down
+  37: [-1, 0], // left
+  39: [1, 0] // right
+};
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
+const App = () => {
+  const canvasRef = useRef();
+  const [snake, setSnake] = useState(SNAKE_START);
+  const [apple, setApple] = useState(APPLE_START);
+  const [dir, setDir] = useState([0, -1]);
+  const [speed, setSpeed] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+
+  useInterval(() => gameLoop(), speed);
+
+  const endGame = () => {
+    setSpeed(null);
+    setGameOver(true);
   };
 
-  const [startGame, setStartGame] = useState(true);
+  const moveSnake = ({ keyCode }) =>
+    keyCode >= 37 && keyCode <= 40 && setDir(DIRECTIONS[keyCode]);
+
+  const createApple = () =>
+    apple.map((_a, i) => Math.floor(Math.random() * (CANVAS_SIZE[i] / SCALE)));
+
+  const checkCollision = (piece, snk = snake) => {
+    if (
+      piece[0] * SCALE >= CANVAS_SIZE[0] ||
+      piece[0] < 0 ||
+      piece[1] * SCALE >= CANVAS_SIZE[1] ||
+      piece[1] < 0
+    )
+      return true;
+
+    for (const segment of snk) {
+      if (piece[0] === segment[0] && piece[1] === segment[1]) return true;
+    }
+    return false;
+  };
+
+  const checkAppleCollision = newSnake => {
+    if (newSnake[0][0] === apple[0] && newSnake[0][1] === apple[1]) {
+      let newApple = createApple();
+      while (checkCollision(newApple, newSnake)) {
+        newApple = createApple();
+      }
+      setApple(newApple);
+      return true;
+    }
+    return false;
+  };
+
+  const gameLoop = () => {
+    const snakeCopy = JSON.parse(JSON.stringify(snake));
+    const newSnakeHead = [snakeCopy[0][0] + dir[0], snakeCopy[0][1] + dir[1]];
+    snakeCopy.unshift(newSnakeHead);
+    if (checkCollision(newSnakeHead)) endGame();
+    if (!checkAppleCollision(snakeCopy)) snakeCopy.pop();
+    setSnake(snakeCopy);
+  };
+
+  const startGame = () => {
+    setSnake(SNAKE_START);
+    setApple(APPLE_START);
+    setDir([0, -1]);
+    setSpeed(SPEED);
+    setGameOver(false);
+  };
+
+  useEffect(() => {
+    const context = canvasRef.current.getContext("2d");
+    context.setTransform(SCALE, 0, 0, SCALE, 0, 0);
+    context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    context.fillStyle = "pink";
+    snake.forEach(([x, y]) => context.fillRect(x, y, 1, 1));
+    context.fillStyle = "lightblue";
+    context.fillRect(apple[0], apple[1], 1, 1);
+  }, [snake, apple, gameOver]);
 
   const router = useRouter();
 
-
+  const handleClick = () => {
+    router.push('/lounge/page');
+  };
 
   return (
-    <>
-      <div className={styles.background}>
+    <><div role="button" tabIndex="0" onKeyDown={e => moveSnake(e)}>
+      <div style={{
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+}}>
+  <canvas
+    style={{ border: "1px solid white" }}
+    ref={canvasRef}
+    width={`${CANVAS_SIZE[0]}px`}
+    height={`${CANVAS_SIZE[0]}px`}
+  />
+
+
+  <div className={styles.background}>
         <div className={styles.content}>
           <div className={styles.content}>
-            <h1>Snake</h1>
             {startGame && (
               <div>
-                <button className={styles["neon-btn"]} onClick={() => setStartGame(!startGame)}>
+                <button className={styles["neon-btn"]} onClick={startGame}>
                   <span className={styles.span}></span>
                   <span className={styles.txt}>START GAME</span>
                 </button>
-
-                <button className={styles["neon-btn"]} onClick={handleClick}>
+                <button className={styles["neon-btn"]} onClick={() => router.push('/lounge/page')}>
                   <span className={styles.span}></span>
                   <span className={styles.txt}>BACK TO LOUNGE</span>
                 </button>
               </div>
             )}
-
-            {!startGame && (
-              <div>
-                test
-              </div>
-            )}
-
           </div>
-          <p className={styles.bottomText}>Made by Marco Spina</p>
+          {gameOver && <div>GAME OVER!</div>}
+          </div>
         </div>
       </div>
+    </div>
     </>
   );
 };
 
-export default Snake;
+export default App;
