d6c47846b591a171a4d3e8e0bc4c223b8cbc66d0
MarcoSpinaBZZ <spinam@bzz.ch>
2023-07-05 09:22:20 +0200

    add calculator jsx

---
+++ s4f-project/pages/calculator/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./calculator.module.css"
+
+function page() {
+  return (
+    <div className={styles.test}>Calculator</div>
+  )
+}
+
+export default page
